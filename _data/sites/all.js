374d3a6d995f10dcaa5d0f35faa3eea306584ef3
Holger Könemann <office@holgerkoenemann.de>
2022-12-23 11:00:15 +0100

    adding abstract

--- _data/sites/all.js
+++ _data/sites/all.js
@@ -5,6 +5,6 @@ module.exports = {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
-		"https://templatedeck.com", "https://stride-html-template.netlify.app/", "https://brooks-html-template.netlify.app/", "https://klar-html-template.netlify.app/"
+		"https://templatedeck.com", "https://stride-html-template.netlify.app/", "https://brooks-html-template.netlify.app/", "https://klar-html-template.netlify.app/", "https://abstract-html-template.netlify.app/"
 	]
 };
